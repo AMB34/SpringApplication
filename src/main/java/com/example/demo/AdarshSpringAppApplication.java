96a4ae569d87d4896830769aa919ab5af901c223
Adarsh Mishra <adarsh@asarsh.mishra>
2018-03-13 21:11:39 +0530

    Initail commit

---
+++ src/main/java/com/example/demo/AdarshSpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdarshSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdarshSpringAppApplication.class, args);
+	}
+}
